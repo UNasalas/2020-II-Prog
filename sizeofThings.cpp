26c7d3b565f4eeb06d9b1e059e01ad593e657c39
ALe <asalas@unal.edu.co>
2020-09-03 01:58:40 +0000

    Tamaño variables

---
+++ sizeofThings.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::cout << "float: " << sizeof(float) << std::endl;
+  std::cout << "double: " << sizeof(double) << std::endl;
+  std::cout << "long double: " << sizeof(long double) << std::endl;
+  return 0;
+}
